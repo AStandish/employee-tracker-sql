bb88dc2e2ec0d59a3400127e269f4af8eca95d0e
AStandish <112442942+AStandish@users.noreply.github.com>
2023-02-01 20:30:16 -0500

    added readme

--- db/seed.sql
+++ db/seed.sql
@@ -1,11 +1,16 @@
 USE employees_db;
 INSERT INTO department (name)
-VALUES ("sales"), ("engineer");
+VALUES ("sales"), ("engineer"), ("accounting"), ("info tech");
 INSERT INTO role (title, salary, department_id)
 VALUES 
 ("sales person", 32000, 1), 
-("engineer person", 70000, 2);
+("engineer person", 70000, 2),
+("accountant", 65000, 3),
+("system admin", 65000, 4);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
 ("Amber", "Standish", 1, NULL),
-("Alora", "Perry", 2, 1);
+("Alora", "Perry", 2, 1),
+("Phoebe", "Phoenix", 3, 2),
+("Stella", "Blue", 4, 3),
+("Ellie", "Gyles", 5, 4);
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { prompt } = require("inquirer");
+const inquirer = require("inquirer");
 const db = require("./db");
+const connection = require("./db/connection");
 
 function runMenu() {
   inquirer
@@ -93,7 +94,7 @@ function viewAllRoles() {
 
 function viewAllEmployees() {
   const sql =
-    'Select emp.id as EmployeeID, concat(emp.first_name,"  ",emp.last_name ) as EmployeeName , ro.title as Job_tittle, ro.salary as Salary,dept.name as Department_Name,concat(emp2.first_name,"  ",emp2.last_name) as ManagerName from employee_tracker.employee as emp left join employee_tracker.employee as emp2 on emp2.id=emp.manager_id left join employee_tracker.Role as ro on emp.role_id=ro.id left join employee_tracker.department as dept on dept.id = ro.department_id';
+    'Select emp.id as EmployeeID, concat(emp.first_name,"  ",emp.last_name ) as EmployeeName , ro.title as Job_tittle, ro.salary as Salary,dept.name as Department_Name,concat(emp2.first_name,"  ",emp2.last_name) as ManagerName from employees_db.employee as emp left join employees_db.employee as emp2 on emp2.id=emp.manager_id left join employees_db.Role as ro on emp.role_id=ro.id left join employees_db.department as dept on dept.id = ro.department_id';
   connection.query(sql, (err, res) => {
     if (err) {
       throw err;
